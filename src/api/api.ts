5f13f27c7ec4fd32a09fac93c6fae4b467561d66
Andrei <rul>
2021-11-08 01:13:46 +0700

    refactor api axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,27 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.headers['Content-Type'] = "application/json;charset=utf-8";
 
 const ServerAuth = "https://maagserver/AuthServer/";
 //const ServerAuth = "https://localhost:44397/";
 
-//const ServerHorcruxMemories = "https://localhost:44370/";
 const ServerHorcruxMemories = "https://maagserver/API_HorcruxMemories/";
+//const ServerHorcruxMemories = "https://localhost:44370/";
+const AuthGuery = axios.create({
+    withCredentials : true,
+    baseURL : ServerAuth,
+    headers : {
+        'Content-Type': "application/json;charset=utf-8"
+    }
+});
+const DataGuery = axios.create({
+    withCredentials : true,
+    baseURL : ServerHorcruxMemories,
+    headers : {
+        'Content-Type': "application/json;charset=utf-8"
+    }
+})
 
-
-const url_register = ServerAuth+"api/user/register";
-const url_token = ServerAuth+"api/user/token";
-const url_AddDeleteRoles = ServerAuth+"api/user/add-delete-roles";
-const url_refresh_token = ServerAuth+"api/user/refresh-token";
-const url_revoke_token = ServerAuth+"api/user/revoke-token";
-const url_user_tokens = ServerAuth+"api/user/tokens/";
-const url_GetUsers = ServerAuth+"api/user/GetUsers";
 function getCookie(name:string) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -30,36 +36,47 @@ function setResponseCookie(data :any){
     document.cookie = `Token=${data.token}; expires=` + dateToken;
 }
 
+type ResponseTokenType = {
+    email: string
+    isAuthenticated: boolean
+    message: string|null
+    refreshToken: string
+    refreshTokenExpiration: string
+    roles: string[]
+    token: string
+    userName: string
+}
+
+enum ResultCodesEnum {
+    Success=0,
+    Error = 1,
+}
+
+
+
 export const AuthAPI = {
     Token : (Email:string,Password:string) => {
-        return  axios({
-            method: 'POST',
-            url:url_token,
-            data: JSON.stringify({Email,Password})
-        }).then(response =>{
-            //let date = new Date(Date.now() + 86400e3).toUTCString();
-            setResponseCookie(response.data)
+        let data = JSON.stringify({Email,Password});
+        return AuthGuery.post<ResponseTokenType>('api/user/token',data)
+            .then(response =>{
+                setResponseCookie(response.data)
                 return response;
-        })
-    },
-    Register : (FirstName:string,LastName:string,Username:string,Email:string,Password:string) => {
-        return axios({
-            method: 'POST',
-            url:url_register,
-            data: JSON.stringify({FirstName,LastName,Username,Email,Password})
-        })
+            })
     },
     RefreshToken : ()=>{
         let RefreshToken=getCookie("RefreshToken");
-        return  axios({
-            method: 'POST',
-            url:url_refresh_token,
-            data: JSON.stringify({RefreshToken})
-        }).then(response =>{
+        let data = JSON.stringify({RefreshToken});
+        return  AuthGuery.post<ResponseTokenType>("api/user/refresh-token",data).then(response =>{
             setResponseCookie(response.data)
             return response;
         })
     },
+    Register : (FirstName:string,LastName:string,Username:string,Email:string,Password:string) => {
+        let data =  JSON.stringify({FirstName,LastName,Username,Email,Password});
+        return  AuthGuery.post("api/user/register",data)
+
+    },
+
     IsExistsToken:()=>{
         let promise = new Promise((resolve:any,reject) => {
             let Token=getCookie("Token");
@@ -73,107 +90,84 @@ export const AuthAPI = {
 
     AddDeleteRole : (Email:string,Roles:string[])=>{
         return  AuthAPI.IsExistsToken().then(req=>{
-            let Token=getCookie("Token");
-            return  axios({
-                method: 'POST',
-                url:url_AddDeleteRoles,
+            let Token = getCookie("Token");
+            let data =  JSON.stringify({Email,Roles});
+            let config = {
                 headers: {
                     'Authorization': `Bearer  ${Token}`
                 },
-                data: JSON.stringify({Email,Roles})
-            })
+            }
+            return AuthGuery.post('api/user/add-delete-roles',data,config)
         })
     },
     RevokeToken : ()=>{
         return AuthAPI.IsExistsToken().then(req=>{
             let RefreshToken=getCookie("RefreshToken");
-            return  axios({
-                method: 'POST',
-                url:url_revoke_token,
-                data: JSON.stringify({RefreshToken})
-            })
+            let data =  JSON.stringify({RefreshToken})
+            return AuthGuery.post('api/user/revoke-token',data)
         })
     },
     GetUser:()=>{
         return AuthAPI.IsExistsToken().then(req=>{
             let Token=getCookie("Token");
-            return  axios({
-                method: 'POST',
+            let config = {
                 headers: {
-                    'Authorization': `Bearer  ${Token}`,
+                    'Authorization': `Bearer  ${Token}`
                 },
-                url:url_GetUsers,
-            })
+            }
+            return AuthGuery.post('api/user/GetUsers',null,config)
         });
 
     },
     UserTokens : (Token:string,id:string)=>{
-        return  axios({
-            method: 'POST',
-            url:url_user_tokens+id,
+        let config = {
             headers: {
-                'Authorization': `Bearer  ${Token}`,
-            }
-        })
+                'Authorization': `Bearer  ${Token}`
+            },
+        }
+        return AuthGuery.post('api/user/tokens/',null,config)
     },
 }
 
-const url_Question_Portions = ServerHorcruxMemories+"api/Question/Portions/";
-const url_Question_Test = ServerHorcruxMemories+"api/Test/";
 export const DataAPI = {
     Portions : (IdParent?:string, Page?:number, PortionsSize?:number )=>{
         return AuthAPI.IsExistsToken().then(req=>{
             let Token=getCookie("Token");
-            return  axios({
-                method: 'GET',
-                url:url_Question_Portions,
+            let config = {
                 headers: {
-                    'Authorization': `Bearer  ${Token}`,
+                    'Authorization': `Bearer  ${Token}`
                 },
                 params: {IdParent, Page, PortionsSize}
-            })
+            }
+            return DataGuery.get('api/Question/Portions/',config)
         });
 
     },
     TestStart : (IdRoot:string,nameTest:string)=>{
         return AuthAPI.IsExistsToken().then(req=>{
             let Token=getCookie("Token");
-            return  axios({
-                method: 'GET',
-                url:url_Question_Test,
+            let config = {
                 headers: {
-                    'Authorization': `Bearer  ${Token}`,
+                    'Authorization': `Bearer  ${Token}`
                 },
                 params: {IdRoot,Type:nameTest}
-            })
+            }
+            return DataGuery.get('api/Test/',config)
         });
     },
     TestNext : (IdRoot:string,TestHistory:string[],id:string,isIgnoreTest:boolean,nameTest:string)=>{
         return AuthAPI.IsExistsToken().then(req=>{
             let Token=getCookie("Token");
-
-
-            return  axios({
-                method: 'POST',
-                url:url_Question_Test,
+            let data= JSON.stringify({IdRoot,TestHistory,id,isIgnoreTest})
+            let config = {
                 headers: {
-                    'Authorization': `Bearer  ${Token}`,
+                    'Authorization': `Bearer  ${Token}`
                 },
                 params:{Type:nameTest},
-                data: JSON.stringify({IdRoot,TestHistory,id,isIgnoreTest})
-            })
+
+            }
+            return DataGuery.post('api/Test/',data,config);
         });
     },
 
 }
-
-
-
-
-
-
-
-
-
-
-
